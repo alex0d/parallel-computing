c47eb0dc1c01b9c9d638f553261fee0c58f875fb
alex0d <alexey.d707@gmail.com>
2022-05-15 18:17:39 +0300

    Variable number of iterations

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import concurrent.futures
-from math import ceil
 from time import time
 
 import loky
 import openturns as ot
 import sympy
 from sympy.utilities.lambdify import lambdify
-
-
-TOTAL_ITERATIONS = 10_000_000
 
 
 def monte_carlo(f, a: int, b: int, iters: int) -> float:
@@ -30,24 +26,23 @@ if __name__ == "__main__":
     integrate_from, integrate_to = map(int, input("Границы интегрирования (a, b): ").split())
 
     processes = int(input("Количество запускаемых процессов (0 - авто): ")) or loky.cpu_count()
-    TOTAL_ITERATIONS = ceil(TOTAL_ITERATIONS / processes) * processes
-    per_process = TOTAL_ITERATIONS // processes
+    total_iterations = int((integrate_to - integrate_from) * 10_000)
+    per_process = total_iterations // processes
 
-    executor = loky.get_reusable_executor(processes)
-
-    res = 0
+    total_sum = 0
     futures = []
 
     print(f"Вычисление запущено...")
     start = time()
-    for i in range(processes):
-        futures.append(executor.submit(monte_carlo, f=f, a=integrate_from, b=integrate_to, iters=per_process))
+    with loky.get_reusable_executor(processes) as executor:
+        for i in range(processes):
+            futures.append(executor.submit(monte_carlo, f=f, a=integrate_from, b=integrate_to, iters=per_process))
 
-    for future in concurrent.futures.as_completed(futures):
-        res += future.result()
+        for future in concurrent.futures.as_completed(futures):
+            total_sum += future.result()
     end = time()
 
-    average_result = (integrate_to - integrate_from) * (res / TOTAL_ITERATIONS)
+    average_result = (integrate_to - integrate_from) * (total_sum / total_iterations)
 
     print(f"\nОпределённый интеграл = {average_result}")
     print(f"Процессов: {processes}. Общее время вычисления: {(end - start):.3f} с")
