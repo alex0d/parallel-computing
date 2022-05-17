7b16f459a1c7a74d844bf00a5ee6163046b413a9
alex0d <alexey.d707@gmail.com>
2022-05-18 01:01:50 +0300

    v2: numexpr & multiprocessing

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
-import concurrent.futures
+from math import ceil, floor, log2
+from multiprocessing import Pool, cpu_count
 from time import time
 
-import loky
-import openturns as ot
-import sympy
-from sympy.utilities.lambdify import lambdify
+from numexpr import evaluate, re_evaluate
+from scipy.stats.qmc import Sobol
 
 
-def monte_carlo(f, a: int, b: int, iters: int) -> float:
-    """ Return sum of the values of function `f` from `a` to `b` over `iters` iterations.
+def monte_carlo(f: str, a: int, b: int, iters_log2: int) -> float:
+    """ Return sum of the values of function `f` from `a` to `b` over `2^iters` iterations.
     """
-    sequence = ot.SobolSequence(1).generate(iters)
+    sequence = Sobol(1).random_base2(iters_log2)
     f_sum = 0
+    evaluate(f, local_dict={'x': a + (b - a) * 0.5})
     for r in sequence:
         x = a + (b - a) * r[0]
-        f_sum += f(x)
+        f_sum += re_evaluate()
     return f_sum
 
 
 if __name__ == "__main__":
-    f = input("Подынтегральная функция: ")
-    f = sympy.simplify(f)
-    f = lambdify(['x'], f)
+    expr = input("Подынтегральная функция: ")
 
     integrate_from, integrate_to = map(int, input("Границы интегрирования (a, b): ").split())
 
-    processes = int(input("Количество запускаемых процессов (0 - авто): ")) or loky.cpu_count()
-    total_iterations = int((integrate_to - integrate_from) * 10_000)
-    per_process = total_iterations // processes
+    processes = int(input("Количество запускаемых процессов (0 - авто): ")) or cpu_count()
+    if processes > 32:
+        print("Количество процессов уменьшено до 32")
+        processes = 32
 
-    total_sum = 0
-    futures = []
+    total_iterations = int((integrate_to - integrate_from) * 4096)
+    per_process_log2 = floor(log2(total_iterations)) - ceil(log2(processes))
+    total_iterations = 2 ** per_process_log2 * processes
 
     print(f"Вычисление запущено...")
     start = time()
-    with loky.get_reusable_executor(processes) as executor:
-        for i in range(processes):
-            futures.append(executor.submit(monte_carlo, f=f, a=integrate_from, b=integrate_to, iters=per_process))
-
-        for future in concurrent.futures.as_completed(futures):
-            total_sum += future.result()
+    with Pool(processes) as pool:
+        results = pool.starmap(monte_carlo,
+                               ((expr, integrate_from, integrate_to, per_process_log2)
+                                for i in range(processes)))
     end = time()
 
-    average_result = (integrate_to - integrate_from) * (total_sum / total_iterations)
+    result = (integrate_to - integrate_from) * (sum(results) / total_iterations)
 
-    print(f"\nОпределённый интеграл = {average_result}")
+    print(f"\nОпределённый интеграл = {result}")
     print(f"Процессов: {processes}. Общее время вычисления: {(end - start):.3f} с")
--- test.py
+++ test.py
@@ -1,49 +1,43 @@
-import concurrent.futures
-from math import ceil
+from math import ceil, floor, log2
+from multiprocessing import Pool, cpu_count
 from time import time
 
-import loky
-import openturns as ot
-import sympy
-from sympy.utilities.lambdify import lambdify
+from numexpr import evaluate, re_evaluate
+from scipy.stats.qmc import Sobol
+
+integrate_from = -1000
+integrate_to = 1000
+FUNCTION_STR = "x**2 + sin(x)"
 
 
-TOTAL_ITERATIONS = 10_000_000
-INTEGRATE_FROM = 10
-INTEGRATE_TO = 15
-FUNCTION_STR = "sin(x) / (sin(x)**2 - 4 * sin(x) * cos(x) + 5 * cos(x)**2)"
-
-def monte_carlo(f, a: int, b: int, iters: int) -> float:
-    """ Return sum of the values of function `f` from `a` to `b` over `iters` iterations.
+def monte_carlo(f: str, a: int, b: int, iters_log2: int) -> float:
+    """ Return sum of the values of function `f` from `a` to `b` over `2^iters` iterations.
     """
-    sequence = ot.SobolSequence(1).generate(iters)
+    sequence = Sobol(1).random_base2(iters_log2)
     f_sum = 0
+    evaluate(f, local_dict={'x': 1.0})
     for r in sequence:
         x = a + (b - a) * r[0]
-        f_sum += f(x)
+        f_sum += re_evaluate()
     return f_sum
 
 
 if __name__ == "__main__":
-    f = sympy.simplify(FUNCTION_STR)
-    f = lambdify(['x'], f)
-    print(f"cpu_count = {loky.cpu_count()}\n")
+    print(f"cpu_count = {cpu_count()}\n")
 
-    for processes in [1, 2, 3, 4, 5, 6, 8, 9, 12, 15, 16, 24, 48]:
-        TOTAL_ITERATIONS = ceil(TOTAL_ITERATIONS / processes) * processes
-        per_process = TOTAL_ITERATIONS // processes
-
-        executor = loky.get_reusable_executor(processes)
-
-        res = 0
-        futures = []
+    for processes in [1, 2, 4, 8, 16, 32]:
+        total_iterations = int((integrate_to - integrate_from) * 4096)
+        per_process_log2 = floor(log2(total_iterations)) - ceil(log2(processes))
+        total_iterations = 2 ** per_process_log2 * processes
 
         start = time()
-        for i in range(processes):
-            futures.append(executor.submit(monte_carlo, f=f, a=INTEGRATE_FROM, b=INTEGRATE_TO, iters=per_process))
-
-        for future in concurrent.futures.as_completed(futures):
-            res += future.result()
+        with Pool(processes) as pool:
+            results = pool.starmap(monte_carlo,
+                                   ((FUNCTION_STR, integrate_from, integrate_to, per_process_log2)
+                                    for i in range(processes)))
         end = time()
 
-        print(f"Processes: {processes:>2}. Time: {(end - start):.3f} s")
+        # result = (integrate_to - integrate_from) * (sum(results) / total_iterations)
+        #
+        # print(f"\nОпределённый интеграл = {result}")
+        print(f"Процессов: {processes}. Общее время вычисления: {(end - start):.3f} с")
